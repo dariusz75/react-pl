1495bb46707a17510ea4cb5e0cd8d061a38cf551
Dariusz Franczak <darius32@op.pl>
2018-10-13 15:33:24 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import Countdown from './Countdown';
 
 const App = () => (
--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import propTypes from 'prop-types';
+
 
 const Countdown = (props) => (
   <p><strong>{props.name}</strong> {props.time}</p>
 );
+
+Countdown.propTypes = {
+  name: propTypes.string,
+  time: propTypes.string
+}
 
 export default Countdown;
