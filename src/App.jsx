a3f339b0e8f6ad24b74061ddb446d323902285da
Dariusz Franczak <darius32@op.pl>
2018-10-14 07:56:32 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 
 import Countdown from './Countdown';
 
-const App = () => (
-  <div>
-    <Countdown name="Breakfast" time="8.00" />
-    <Countdown name="Lunch" time="14.00" />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      events: [
+        { id: 0, name: 'Breakfast', time: '7.00' },
+        { id: 1, name: 'Lunch', time: '13.00' },
+        { id: 2, name: 'Afternoon Tea', time: '17.00' }
+      ]
+    }
+  }
+
+
+
+  render() {
+
+    let events = this.state.events.map(el => {
+      return <Countdown key={el.id} name={el.name} time={el.time} />;
+    })
+
+    return (
+      <div>
+        {events}
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
