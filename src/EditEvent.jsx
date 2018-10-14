9af652a79b91575ce149f508c455391233daa26f
Dariusz Franczak <darius32@op.pl>
2018-10-14 09:18:35 +0100

    more fields added

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -4,9 +4,19 @@ const EditEvent = () => {
 
   return <div className="edit-event">
     <div className="edit-event__input-group">
-      <label htmlFor="name" >Name</label>
+      <label htmlFor="name">Name</label>
       <input id="name" type="text" name="name" />
     </div>
+    <div className="edit-event__input-group">
+      <label htmlFor="hour">Hour</label>
+      <input id="hour" type="tel" name="hour" />
+    </div>
+    <div className="edit-event__input-group">
+      <label htmlFor="minutes">Minutes</label>
+      <input id="minutes" type="tel" name="minutes" />
+    </div>
+    <button>OK</button>
+    <button>Cancel</button>
   </div>;
 };
 
