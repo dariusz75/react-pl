c6014f492aa80316649ca7c2aeed8cdd92321da4
Dariusz Franczak <darius32@op.pl>
2018-10-15 21:25:51 +0100

    completed

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -7,15 +7,33 @@ const EditEvent = (props) => {
   return <div className="edit-event">
     <div className="edit-event__input-group">
       <label className="edit-event__input-group__label" htmlFor="name">Name</label>
-      <input id="name" className="edit-event__input-group__input" type="text" name="name" />
+      <input
+        id="name"
+        className="edit-event__input-group__input"
+        type="text"
+        name="name"
+        onChange={(e) => console.log(e.target.value)}
+      />
     </div>
     <div className="edit-event__input-group">
       <label className="edit-event__input-group__label" htmlFor="hour">Hour</label>
-      <input id="hour" className="edit-event__input-group__input" type="tel" name="hour" />
+      <input
+        id="hour"
+        className="edit-event__input-group__input"
+        type="tel"
+        name="hour"
+        onChange={(e) => console.log(e.target.value)}
+      />
     </div>
     <div className="edit-event__input-group">
       <label className="edit-event__input-group__label" htmlFor="minutes">Minutes</label>
-      <input id="minutes" className="edit-event__input-group__input" type="tel" name="minutes" />
+      <input
+        id="minutes"
+        className="edit-event__input-group__input"
+        type="tel"
+        name="minutes"
+        onChange={(e) => console.log(e.target.value)}
+      />
     </div>
     <div className="edit-event__input-group">
       <button className="edit-event__input-group__button" onClick={() => alert('Test A')} >OK</button>
